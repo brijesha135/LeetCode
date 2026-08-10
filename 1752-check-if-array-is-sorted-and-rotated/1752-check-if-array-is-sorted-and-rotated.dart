90433a599742cb410aa6042656c94514d26bbcca
brijesha135 <140812032+brijesha135@users.noreply.github.com>
2026-08-10 13:13:49 +0530

    Time: 0 ms (100%), Space: 148.9 MB (28.57%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.dart
@@ -0,0 +1,9 @@
+class Solution {
+  bool check(List<int> nums) {
+    int count = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[(i + 1) % nums.length]) count++;
+    }
+    return count <= 1;
+  }
+}
