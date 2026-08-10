d73716e6ab09243373a8fd015c9a5c3873b5cb7f
brijesha135 <140812032+brijesha135@users.noreply.github.com>
2026-08-10 13:01:18 +0530

    Time: 178 ms (100%), Space: 158.9 MB (100%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.dart
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.dart
@@ -2,29 +2,44 @@ class Solution {
   List<int> getBiggestThree(List<List<int>> grid) {
     int m = grid.length;
     int n = grid[0].length;
-    Set<int> sums = {};
-
+    Set<int> sums = <int>{};
+    
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
         sums.add(grid[i][j]);
-
-        for (int k = 1; i + 2 * k < m && j + k < n && j - k >= 0; k++) {
+        
+        for (int k = 1; i - k >= 0 && i + k < m && j - k >= 0 && j + k < n; k++) {
           int sum = 0;
-          for (int step = 0; step <= k; step++) {
-            sum += grid[i + step][j - step];
-            sum += grid[i + step][j + step];
-            sum += grid[i + 2 * k - step][j - step];
-            sum += grid[i + 2 * k - step][j + step];
+          
+          sum += grid[i - k][j];
+          
+          for (int step = 1; step < k; step++) {
+            sum += grid[i - k + step][j + step];
           }
-          sum -= grid[i][j];
-          sum -= grid[i + 2 * k][j];
-          sum -= grid[i + k][j - k];
-          sum -= grid[i + k][j + k];
+          
+          sum += grid[i][j + k];
+          
+          for (int step = 1; step < k; step++) {
+            sum += grid[i + step][j + k - step];
+          }
+          
+          sum += grid[i + k][j];
+          
+          for (int step = 1; step < k; step++) {
+            sum += grid[i + k - step][j - step];
+          }
+          
+          sum += grid[i][j - k];
+          
+          for (int step = 1; step < k; step++) {
+            sum += grid[i - step][j - k + step];
+          }
+          
           sums.add(sum);
         }
       }
     }
-
+    
     List<int> sorted = sums.toList()..sort((a, b) => b.compareTo(a));
     return sorted.length <= 3 ? sorted : sorted.sublist(0, 3);
   }
